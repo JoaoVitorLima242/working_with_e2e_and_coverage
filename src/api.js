2226d4c2abc437ed85621c151852a3e58e675279
C5363386 <joao.vitor.brietzke.de.lima@sap.com>
2023-12-07 17:51:48 -0300

    feat: starting login route

--- src/api.js
+++ src/api.js
@@ -1,10 +1,20 @@
 const http = require("http");
+const { once } = require("events");
 
 const PORT = 3000;
+const DEFAULT_USER = {
+  username: "Joaozin",
+  password: "123",
+};
 
 const routes = {
   "/contact:get": (req, res) => {
     res.write("Contact Us Page");
+    return res.end();
+  },
+  "/login:post": async (req, res) => {
+    const { username, password } = JSON.parse(await once(req, "data"));
+
     return res.end();
   },
   notFound: (req, res) => {
@@ -26,3 +36,9 @@ const handler = (req, res) => {
 const app = http
   .createServer(handler)
   .listen(PORT, () => console.log("Running at " + PORT));
+
+/* 
+    ROUTE LOGIN = curl -X POST --data '{"username": "username", "password": "1234"}'  localhost:3000/login
+    ROUTE CONTACT = curl localhost:3000/contact
+
+*/
