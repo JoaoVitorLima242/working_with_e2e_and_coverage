565d7a53176896d8c8b95187f8bbe34a8f195f01
C5363386 <joao.vitor.brietzke.de.lima@sap.com>
2023-12-07 17:42:31 -0300

    feat: adding not found route

--- src/api.js
+++ src/api.js
@@ -3,19 +3,22 @@ const http = require("http");
 const PORT = 3000;
 
 const routes = {
-    '/contact:get': (req, res) => {
-        res.write('Contact Us Page')
-        return res.end()
-    },
-
-}
+  "/contact:get": (req, res) => {
+    res.write("Contact Us Page");
+    return res.end();
+  },
+  notFound: (req, res) => {
+    res.writeHead(404);
+    return res.end("Not found");
+  },
+};
 
 const handler = (req, res) => {
   const { url, method } = req;
 
-  const routeKey = `${url.toLowerCase()}:${method.toLowerCase()}`
+  const routeKey = `${url.toLowerCase()}:${method.toLowerCase()}`;
 
-  const chosen = routes[routeKey]
+  const chosen = routes[routeKey] || routes.notFound;
 
   return chosen(req, res);
 };
